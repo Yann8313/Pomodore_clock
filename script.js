8d9dd1932d87a915bfb82cf7592c12d538ef0642
Yannick Ayassa <yayassa@Mac435.local>
2020-12-16 14:10:52 -0500

    FinishFix

--- script.js
+++ script.js
@@ -2,83 +2,106 @@
  VARIABLES DOM
  ********************************/
 
-const timeLeftDOM = document.getElementById('timeleft');
-const playButton = document.getElementById('buttonPlay');
-const pauseButton = document.getElementById('BtnPause');
-const resetButton = document.getElementById('buttonReset');
-const sessionLengthDOM = document.getElementById('sessionLength');
-const sessionIncrement = document.getElementById('sessionIncrement');
-const sessionDecrement = document.getElementById('sessionDecrement');
-const breakLengthDOM = document.getElementById('breakLength');
-const breakIncrement = document.getElementById('breakIncrement');
-const breakDecrement = document.getElementById('breakDecrement');
-const relaxSong = document.getElementById('relaxSong');
+const timeLeftDOM = document.getElementById("timeleft");
+const playButton = document.getElementById("buttonPlay");
+const pauseButton = document.getElementById("BtnPause");
+const resetButton = document.getElementById("buttonReset");
+const sessionLengthDOM = document.getElementById("sessionLength");
+const sessionIncrement = document.getElementById("sessionIncrement");
+const sessionDecrement = document.getElementById("sessionDecrement");
+const breakLengthDOM = document.getElementById("breakLength");
+const breakIncrement = document.getElementById("breakIncrement");
+const breakDecrement = document.getElementById("breakDecrement");
+const relaxSong = document.getElementById("relaxSong");
+const timeBreak = parseInt(breakLengthDOM.innerText);
+const timeWork = parseInt(sessionLengthDOM.innerText);
 
+
+let interval = null;
+let valueTimer = 0;
 let valueSession = 25;
 let valueBreak = 5;
 let sec = 59;
-let interval = null;
+let breakTime = false;
+let workTime = true;
+
+
 
 class Countdowm {
   constructor() {
-    playButton.addEventListener('click', this.startTimer);
-    pauseButton.addEventListener('click', this.stopTimer);
-    resetButton.addEventListener('click', this.resetTimer);
+    playButton.addEventListener("click", this.startTimer);
+    pauseButton.addEventListener("click", this.stopTimer);
+    resetButton.addEventListener("click", this.resetTimer);
   }
 
   startTimer() {
     interval = setInterval(() => {
       if (sec < 10) {
-        sec = '0' + sec;
+        sec = "0" + sec;
       }
-      timeLeftDOM.innerHTML = valueSession - 1 + ' : ' + sec;
+      workTime ? (valueTimer = valueSession) : (valueTimer = valueBreak);
+      timeLeftDOM.innerHTML = valueTimer - 1 + " : " + sec;
       sec--;
-      if (sec === 0) {
+
+      if (workTime && sec === 0) {
         valueSession--;
         sec = 59;
-        if (valueSession === 0) {
-          relaxSong.play();
-          valueSession = valueBreak;
-        }
-        if (valueBreak === 0) {
-          valueBreak = valueSession;
-        }
       }
-      playButton.classList.add('hide');
+
+      if (valueSession === 0) {
+        breakTime = true;
+        workTime = false;
+        valueSession = timeWork;
+        relaxSong.play();
+      }
+
+      if (breakTime && sec === 0) {
+        valueBreak--;
+        sec = 59;
+      }
+      if (valueBreak === 0) {
+        breakTime = false;
+        workTime = true;
+        valueBreak = timeBreak;
+        relaxSong.play();
+      }
+
+      playButton.classList.add("hide");
     }, 1000);
   }
 
   stopTimer() {
-    playButton.classList.remove('hide');
+    playButton.classList.remove("hide");
     clearInterval(interval);
   }
 
   resetTimer() {
-    valueBreak = 5;
     valueSession = 25;
+    valueBreak = 5;
     sec = 59;
     sessionLengthDOM.innerHTML = 25;
     breakLengthDOM.innerHTML = 5;
     clearInterval(interval);
-    timeLeftDOM.innerHTML = valueSession + ' : ' + '0' + 0;
-    playButton.classList.remove('hide');
+    timeLeftDOM.innerHTML = valueSession + " : " + "0" + 0;
+    playButton.classList.remove("hide");
     new SessionAndBreakLength(valueSession, valueBreak);
   }
 }
+
 
 class SessionAndBreakLength {
   constructor(valueSession, valueBreak) {
     (this.valueBreak = valueBreak),
       (this.valueSession = valueSession),
       this.incrementDecrementValue(
-        'Break',
+        "Break",
         valueBreak,
         breakLengthDOM,
         breakIncrement,
         breakDecrement
       );
     this.incrementDecrementValue(
-      'Session',
+      "Session",
       valueSession,
       sessionLengthDOM,
       sessionIncrement,
@@ -93,14 +116,14 @@ class SessionAndBreakLength {
     sectionIncrement,
     sectionDecrement
   ) {
-    sectionIncrement.addEventListener('click', () => {
+    sectionIncrement.addEventListener("click", () => {
       if (value >= 60) {
         return;
       }
       lenghtDOM.innerHTML = ++value;
       this.updateCountdown(flag, value);
     });
-    sectionDecrement.addEventListener('click', () => {
+    sectionDecrement.addEventListener("click", () => {
       if (value <= 1) {
         return;
       }
@@ -112,10 +135,10 @@ class SessionAndBreakLength {
   // Update Countdowm in the DOM
 
   updateCountdown(flag, value) {
-    playButton.classList.remove('hide');
+    playButton.classList.remove("hide");
     clearInterval(interval);
-    flag === 'Session' ? (valueSession = value) : (valueBreak = value);
-    timeLeftDOM.innerHTML = valueSession + ' : ' + '00';
+    flag === "Session" ? (valueSession = value) : (valueBreak = value);
+    timeLeftDOM.innerHTML = valueSession + " : " + "00";
   }
 }
 
